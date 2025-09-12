c9bfd9da76c1eaab2ae90140d49844eccd9ad386
mac <hangxi.lin@mail.utoronto.ca>
2025-09-11 21:04:36 -0400

    change for loop to while loop, and add Main.Java, add new multiple.Java, and add the test file to the test folder.

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,19 +2,18 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        System.out.println(multiples());
+        System.out.println(multiples(1000, 3, 5));
     }
-
-    public static int multiples() {
+    public static int multiples(int n, int a, int b) {
         int count = 0;
         int i = 1;
-        while (i < 1000) {
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-            if (divisibleBy3 || divisibleBy5){
-                count ++;
+        while (i < n) {
+            boolean divisibleByA = i % a == 0;
+            boolean divisibleByB = i % b == 0;
+            if (divisibleByA || divisibleByB) {
+                count++;
             }
-            i ++;
+            i++;
         }
         return count;
     }
